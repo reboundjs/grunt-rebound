8194f2d570464ce5c6660d840f624f3cf98c388c
Adam Miller <adam@pages.me>
2014-09-09 23:15:24 -0700

    Bump to new version of Rebound - post testing framework version

--- tasks/rebound.js
+++ tasks/rebound.js
@@ -55,7 +55,7 @@ module.exports = function(grunt) {
 
         // Compile
         src = rebound.precompile(src, {
-          filepath: filepath,
+          name: filepath,
           baseDest: options.baseDest,
           baseUrl: options.baseUrl
         });
